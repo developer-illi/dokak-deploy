cdb6b2ca89cc6e492f56f85633491fb15625d0ba
developer-illi <132191529+developer-illi@users.noreply.github.com>
2023-09-12 07:50:37 +0900

    Update settings.py

--- dokak/settings.py
+++ dokak/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 # STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATIC_ROOT = os.path.join('staticfiles')
+STATIC_ROOT = os.path.join('static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
